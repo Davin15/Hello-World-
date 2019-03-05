de602f698637c72203c221054e14a2767170a55c
Davin15 <47573309+Davin15@users.noreply.github.com>
2019-03-05 12:32:48 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created by: Davin Rousseau
+-- Created on: Feb.4th/2019
+-- 
+-- This file prints "Hello, World!" to the console
+-----------------------------------------------------------------------------------------
+
+display.setStatusBar(display.HiddenStatusBar)
+
+print( "Hello, World!")
