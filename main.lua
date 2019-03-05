1726cf0a2a4aac8f50128e3172f4322255ad6476
Davin15 <47573309+Davin15@users.noreply.github.com>
2019-03-05 12:32:15 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created by Davin.R.
+-- Created on Feb.12th,2019
+-- 
+-- This project displays Ninja Boy on Iphone 5
+-----------------------------------------------------------------------------------------
+display.setDefault( "background", 0, 255, 0 )
+
+local image = display.newImageRect( "Assets/Ninja Boy.png", 280, 280 )
+image.x = 150
+image.y = 225
